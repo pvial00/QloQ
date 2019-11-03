35f20d5b87675126f1c5e64cf363aef4cdec4de7
Karl Zander <pvial@Karls-MacBook-Air.local>
2019-11-03 09:31:38 -0800

    Q'loQ keygen

--- src/qloq_keygen.c
+++ src/qloq_keygen.c
@@ -3,9 +3,15 @@
 #include <string.h>
 #include "qloq.h"
 
-int main() {
-    struct qloq_ctx ctx;
+int main(int argc, char *argv[]) {
     int psize = 1536;
+    if (argc < 2) {
+        psize = 1536;
+    }
+    else {
+        psize = atoi(argv[1]);
+    }
+    struct qloq_ctx ctx;
     keygen(&ctx, psize);
     return 0;
 }
